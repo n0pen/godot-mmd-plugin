d7e10bc469846d4baa654cf17e8dc20e5fd10bb2
n0pen <noscandev@protonmail.com>
2026-02-04 20:52:51 -0300

    fix string conversion

--- src/mmd_helpers.cpp
+++ src/mmd_helpers.cpp
@@ -11,7 +11,7 @@ String mmd_helpers::convert_string(const std::string &p_string, uint8_t p_encodi
 		d_string.resize(str_len);
 		const char *str_data = p_string.c_str();
 		if (str_data != nullptr) {
-			memcpy(d_string.ptrw(), str_data, str_len / 2 * sizeof(char16_t));
+			memcpy(d_string.ptrw(), str_data, str_len * sizeof(char));
 		}
 		if (!p_encoding) {
 			return d_string.get_string_from_utf16();
